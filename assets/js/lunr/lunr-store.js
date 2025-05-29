50625f57bd1d70f070d2ddf94ae8199acb10d26a
feisty171 <feisty171@users.noreply.github.com>
2025-05-29 03:50:27 +0000

    deploy: 19d284767528a24f37b62db25487b6b541b2a4f3

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,56 @@
+var store = [{
+        "title": "Hello world",
+        "excerpt":"Hello world 💚  ","categories": [],
+        "tags": [],
+        "url": "https://hofeiying.com/hello-world/",
+        "teaser":null},{
+        "title": "A letter to the person who've hurt me the most",
+        "excerpt":"Dear Dad, It’s your birthday today, and it’s been a long time since I celebrated it with you. I don’t have the courage to send this letter because deep down I think you’re still the same person and wouldn’t react well. I remember when I was a kid, I stopped...","categories": [],
+        "tags": [],
+        "url": "https://hofeiying.com/Dear-Dad/",
+        "teaser":null},{
+        "title": "Conditioned Silence",
+        "excerpt":"Part of growing up is learning who you really are. For me, that process has been painful. I come from a dysfunctional family where I had to manage my emotions alone from a young age. Over time, I learned to repress them. I believed I wasn’t allowed to feel what...","categories": [],
+        "tags": [],
+        "url": "https://hofeiying.com/Conditioned-Silence/",
+        "teaser":null},{
+        "title": "Overcoming Perfectionism and Procrastination",
+        "excerpt":"My boss often talks about the “get things done” mentality, a philosophy that emphasizes action over perfection. When he first introduced this concept to me, I wasn’t sure if it aligned with my values or approach to work. As someone who has always been an overthinker, I’ve long aimed for...","categories": [],
+        "tags": [],
+        "url": "https://hofeiying.com/Overcoming-perfectionism-and-procrastination/",
+        "teaser":null},{
+        "title": "What is The Meaning of Life",
+        "excerpt":"Ever since I was a child, I’ve carried with me a restless curiosity about the world. It stayed with me through my teenage years and now into adulthood. I’ve always been drawn to solving the questions that intrigues me. But one question in particular still lingers unanswered: What is the...","categories": [],
+        "tags": [],
+        "url": "https://hofeiying.com/What-is-the-meaning-of-life/",
+        "teaser":null},{
+        "title": "I don't care paradox",
+        "excerpt":"“I don’t care.” These are the three words I often say to people or mutter under my breath. You’ve probably heard them from your loved ones or friends too. They’re common, way too common. But there’s something ironic about saying them and secretly caring too much. It’s a paradox I...","categories": [],
+        "tags": [],
+        "url": "https://hofeiying.com/I-dont-care-paradox/",
+        "teaser":null},{
+        "title": "On Being Held Captive by My Emotions",
+        "excerpt":"Growing up, I was always held captive by my emotions. I had no real control over my impulses. I would get angry and say things recklessly, often hurting the people I loved. It was like when something triggers you and you feel that tingling in the back of your neck....","categories": [],
+        "tags": [],
+        "url": "https://hofeiying.com/On-Being-Held-Captive-By-My-Emotions/",
+        "teaser":null},{
+        "title": "The Edge of Grief",
+        "excerpt":"I’ve always been an emotional person. Someone who’s deeply sentimental, easily moved by small gestures, old photos, and fading memories. Thankfully, I never had to face real loss when I was young. But as I grow older, the thought of grief feels like a shadow that creeps closer with each...","categories": [],
+        "tags": [],
+        "url": "https://hofeiying.com/The-Edge-of-Grief/",
+        "teaser":null},{
+        "title": "A Life Spent On Self-Sabotage",
+        "excerpt":"I always assumed that the older I got, the closer I’d be to the peace I desperately yearned for as a child. The truth is, peace was never something I could reliably expect. Conflict felt more certain than winning the lottery. If you asked me to name a moment from...","categories": [],
+        "tags": [],
+        "url": "https://hofeiying.com/A-Life-Spent-On-Self-Sabotage/",
+        "teaser":null},{
+        "title": "The Mirage of Greener Grass",
+        "excerpt":"I grew up under the shadow of an abusive and controlling father. My childhood felt like being a bird trapped in a cage, constantly yearning for the sky that was always just beyond my reach. Every daydream revolved around the moment I’d finally gather the courage to break free from...","categories": [],
+        "tags": [],
+        "url": "https://hofeiying.com/The-Mirage-of-Greener-Grass/",
+        "teaser":null},{
+        "title": "The Truth Will Set You Free",
+        "excerpt":"The value of honesty was instilled in me throughout childhood, not just at home but also in classrooms and by figures of authority. Growing up, it was common to hear elders stress the importance of truthfulness. However, honesty wasn’t always simple. My father was intimidating, and even now, memories of...","categories": [],
+        "tags": [],
+        "url": "https://hofeiying.com/The-Truth-Will-Set-You-Free/",
+        "teaser":null}]
